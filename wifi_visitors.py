1f808d7a1cf078edc310e974a655557110871b22
nikolajjsj <nikolaj.app.dev@gmail.com>
2020-09-03 11:24:35 +0200

    Wifi_visitors for identifying visitors to a network

---
+++ wifi_visitors.py
@@ -0,0 +1,16 @@
+import subprocess
+
+ip = "192.168.1.102"
+person = "192.168.1.102:"
+
+proc = subprocess.Popen(["ping", ip], stdout=subprocess.PIPE)
+while True:
+    line = proc.stdout.readline()
+    print(line)
+    if not line:
+        break
+    connected_ip = line.decode('utf-8').split()[3]
+
+    if connected_ip == person:
+        subprocess.Popen(["say", "Person is on the network"])
+        break
